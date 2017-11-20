28d624eb42c1c59c179f32495b2c88be2cc30367
Valerie Jane <valeriejanethoma@gmail.com>
2017-11-20 08:18:33 -0500

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+	$('.scrollspy').scrollSpy({
+		target: '.scrollspy',
+		'scrollOffset': 200
+	});
+	
+	$(".button-collapse").sideNav();
+
+
+
+}); //end doc.ready
