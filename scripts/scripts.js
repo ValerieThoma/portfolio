77350dfe8f7f0f10e4030207682a79fe3289b0cd
Valerie Jane <valeriejanethoma@gmail.com>
2017-11-27 08:33:37 -0500

    update about

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
 
-
+	// SCROLLSPY
 	$('.scrollspy').scrollSpy({
 		target: '.scrollspy',
 		'scrollOffset': 200
 	});
+	// SIDENAV 
+	$(".button-collapse").sideNav({
+		closeOnClick: true
+	});
 	
-	$(".button-collapse").sideNav();
 
+	// ISOTOPE
+	var $grid = $('.grid').isotope({
+
+		});
+		$('#front').click(()=>{
+			$grid.isotope({filter: '.front'});
+		});
+		$('#back').click(()=>{
+			$grid.isotope({filter: '.back'});
+		});
+		$('#dev').click(()=>{
+			$grid.isotope({filter: '.dev'});
+		});	
+		$('#all').click(()=>{
+			$grid.isotope({filter: ''});
+		});
+
+	// TOOLTIPPED
+	$(document).ready(function(){
+    	$('.tooltipped').tooltip({delay: 50});
+ 	});	
 
 
 }); //end doc.ready
